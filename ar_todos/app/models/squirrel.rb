2f65e39c1703b617200af6c052cc01281be28089
Jordan Luse <JLuse@Jordans-MacBook-Air-2.local>
2015-01-30 16:28:05 -0800

    Living SQUIRRELS

--- ar_todos/app/models/squirrel.rb
+++ ar_todos/app/models/squirrel.rb
@@ -10,6 +10,18 @@ class Squirrel < ActiveRecord::Base
     self.update_attributes(death_time: self.world.year)
   end
 
+  def survival_check(challenge, fitness_hash)
+    base = 17000
+    fitness_hash.each do |key, value|
+      has = true
+      key.each do |string|
+        has = has && self.genome_list.include?(string)
+      end
+      base += value if has
+    end
+    dies! if base < challenge
+  end
+
   def age
     self.world.year - self.birth_time
   end
--- ar_todos/app/models/world.rb
+++ ar_todos/app/models/world.rb
@@ -2,6 +2,10 @@ class World < ActiveRecord::Base
 
   has_many :squirrels
 
+    def set_world_year
+      self.update_attributes(year: 0)
+    end
+
     def fitness_hash
       exprs = /((?:\w{2}+?))([pm])(\d+)/
       test =  self.fitness
@@ -16,11 +20,36 @@ class World < ActiveRecord::Base
     fitness_hash
   end
 
-  def make_babies(number)
+  def time_plus
+    self.update_attributes(year: self.year + 1)
+    make_babies
+    challenge = rand(20000)
+    self.squirrels.each do |squirrel|
+      if squirrel.death_time == nil
+        squirrel.survival_check(challenge, self.fitness_hash)
+      end
+    end
+
+
+  end
+
+  def make_founders(population=100)
+    gene_pool = ("aa".."zz").to_a
+    population.times do
+      genes = gene_pool.sample(10)
+      new_genome = genes.join('')
+      self.squirrels.create(genome: new_genome).set_birth_time
+    end
+  end
+
+  def living_squirrels
+    self.squirrels.where(death_time: nil)
+  end
+
+  def make_babies(number=100)
     parents = self.squirrels.where(death_time: nil).sample(number*2)
     parents = parents.each_slice(2).to_a
     parents.each do |family|
-      p family
       break if family.length ==1
       seed0 = family[0].genome_list.sample(5)
       seed1 = family[1].genome_list.sample(5)
